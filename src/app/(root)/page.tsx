912efa9240b322d6e1e87485219908d4e4cc1a88
vvekqls <d3song@ucsd.edu>
2023-08-04 17:22:54 -0700

    [DS] Set up env

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div
+      className="flex 
+    min-h-full 
+    flex-col 
+    justify-center 
+    py-12 sm:px-6 lg:px-8 bg-gray-100"
+    >
+      <p>Hello Messenger!</p>
+    </div>
+  );
+}
