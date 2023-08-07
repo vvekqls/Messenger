06a0afd7bbceab1791e78ff0e439fd504a907566
vvekqls <d3song@ucsd.edu>
2023-08-06 22:59:34 -0700

    [DS] style index

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,18 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="flex">
+    <div
+      className="
+      flex
+      min-h-full
+      flex-col 
+      justify-center
+      py-12
+      sm:px-6
+      lg:px-8
+      bg-gray-100
+    "
+    >
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
           src="/images/logo.png"
